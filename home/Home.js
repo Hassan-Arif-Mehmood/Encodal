2ef07ddcb4819b2d15cb7c089ff893ce46c4887c
Hassan Arif <hassanarif36368@gmail.com>
2025-11-25 15:52:21 +0500

    Home page New Design

---
+++ home/Home.js
@@ -0,0 +1,15 @@
+particlesJS("particles-js", {
+    particles: {
+      number: { value: 80 },
+      size: { value: 3 },
+      color: { value: "#0d6efd" },
+      line_linked: {
+        enable: true,
+        distance: 120,
+        color: "#0d6efd",
+        opacity: 0.4,
+        width: 1
+      },
+      move: { speed: 3 }
+    }
+  });
